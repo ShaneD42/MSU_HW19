005581aa0cf36a2b24fc99b2358df67c02491097
Shane Daykin <shanedaykin92@gmail.com>
2021-02-09 22:23:06 -0500

    Heroku

---
+++ build/precache-manifest.b972b171b41b95183eb842e1be4fc9f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7bbec9922bdcd322d37403c3a5ce48",
+    "url": "/MSU_HW19/index.html"
+  },
+  {
+    "revision": "83bba4732d7d7e36be61",
+    "url": "/MSU_HW19/static/css/main.88b69c29.chunk.css"
+  },
+  {
+    "revision": "c59cbff2334d3fa1064d",
+    "url": "/MSU_HW19/static/js/2.2ba8b590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MSU_HW19/static/js/2.2ba8b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83bba4732d7d7e36be61",
+    "url": "/MSU_HW19/static/js/main.fc3bd568.chunk.js"
+  },
+  {
+    "revision": "9bb741ae54b18e3ecf53",
+    "url": "/MSU_HW19/static/js/runtime-main.6c659c5b.js"
+  }
+]);
