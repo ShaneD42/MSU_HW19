c0b7b3607eae1720ebc9a619b4aa09f6c8e03144
Shane Daykin <shanedaykin92@gmail.com>
2021-02-09 22:11:56 -0500

    Updates

---
+++ precache-manifest.c7e9002223abf4e9e5368f5dbd9750f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dc3eeb3fcf059756b1641adebfc440",
+    "url": "/ShaneD42/MSU_HW19/index.html"
+  },
+  {
+    "revision": "2beff41ba6bd9a0df5ed",
+    "url": "/ShaneD42/MSU_HW19/static/css/main.88b69c29.chunk.css"
+  },
+  {
+    "revision": "c59cbff2334d3fa1064d",
+    "url": "/ShaneD42/MSU_HW19/static/js/2.2ba8b590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ShaneD42/MSU_HW19/static/js/2.2ba8b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2beff41ba6bd9a0df5ed",
+    "url": "/ShaneD42/MSU_HW19/static/js/main.317664b7.chunk.js"
+  },
+  {
+    "revision": "d15383789575bf80d258",
+    "url": "/ShaneD42/MSU_HW19/static/js/runtime-main.69da88da.js"
+  }
+]);
